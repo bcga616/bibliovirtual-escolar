9de1c20d7b925e86951ce14b25941292440df4d2
bcga616 <angelgabrielcb2006@gmail.com>
2025-09-12 16:32:42 -0300

    filtro de busqueda

--- BiblioVirtual/src/App.tsx
+++ BiblioVirtual/src/App.tsx
@@ -4,7 +4,9 @@ import './App.css'
 
 
 function App() {
-  const [libros, setLibros] = useState([])
+
+  const [libros, setLibros] = useState([])  
+  const [busqueda, setBusqueda] = useState('')
 
   useEffect(() => {
     const guardados = localStorage.getItem('libros')
@@ -16,29 +18,44 @@ function App() {
     }
   }, [])
 
+    const librosFiltrados = libros.filter((libro) =>
+    libro.titulo.toLowerCase().includes(busqueda.toLowerCase()) ||
+    libro.autor.toLowerCase().includes(busqueda.toLowerCase())
+  )
+
   return (
     <>
     <div className='container'>
       <h1>Biblioteca</h1>
-      
-      <div className='listalibros'>
-        <table>
-          <th>Titulo</th>
-          <th>Autor</th>
-          <th>Genero</th>
-          <th>Disponibles</th>
-          <th>Total</th>
-          
-          {libros.map((libro) => (
-            <tr key={libro.id}>
-              <td>{libro.titulo}</td>
-              <td>{libro.autor}</td>
-              <td>{libro.categoria}</td>
-              <td>{libro.unidadesDisponibles}</td>
-              <td>{libro.totalUnidades}</td>
-            </tr>
-          ))}
-        </table>
+      <input
+          type="text"
+          placeholder='Buscar por título o autor...'
+          value={busqueda}
+          onChange={(e) => setBusqueda(e.target.value)}
+        />
+        <div className='listalibros'>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Autor</th>
+                <th>Género</th>
+                <th>Disponibles</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {librosFiltrados.map((libro) => (
+                <tr key={libro.id}>
+                  <td>{libro.titulo}</td>
+                  <td>{libro.autor}</td>
+                  <td>{libro.categoria}</td>
+                  <td>{libro.unidadesDisponibles}</td>
+                  <td>{libro.totalUnidades}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
       </div>
     </div>
     </>
